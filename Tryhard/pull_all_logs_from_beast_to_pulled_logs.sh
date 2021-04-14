017e14d62827582500a706d02f1449c1e70df6fd
stefanwanckel <wanckel@gestalt-robotics.com>
2021-04-14 10:38:43 +0200

    tested scaling, tried env 5_3 in enjoy

--- Tryhard/pull_all_logs_from_beast_to_pulled_logs.sh
+++ Tryhard/pull_all_logs_from_beast_to_pulled_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-scp -r 192.168.255.10:/data/stefan_ma/DRL/DRL/Tryhard/logs/her/ur5e_reacher-v2_3/rl_model_390000_steps.zip /home/stefan/Documents/Masterarbeit/DRL/Tryhard/Pulled_logs/
+scp -r 192.168.255.10:/data/stefan_ma/DRL/DRL/Tryhard/logs/her/ur5e_reacher-v5_3/rl_model_580000_steps.zip /home/stefan/Documents/Masterarbeit/DRL/Tryhard/Pulled_logs/
 
-scp -r 192.168.255.10:/data/stefan_ma/DRL/DRL/Tryhard/logs/her/ur5e_reacher-v2_3/evaluations.npz /home/stefan/Documents/Masterarbeit/DRL/Tryhard/Pulled_logs/
+scp -r 192.168.255.10:/data/stefan_ma/DRL/DRL/Tryhard/logs/her/ur5e_reacher-v5_3/evaluations.npz /home/stefan/Documents/Masterarbeit/DRL/Tryhard/Pulled_logs/
 
-scp -r 192.168.255.10:/data/stefan_ma/DRL/DRL/Tryhard/logs/her/ur5e_reacher-v2_3/ur5e_reacher-v2 /home/stefan/Documents/Masterarbeit/DRL/Tryhard/Pulled_logs/
+scp -r 192.168.255.10:/data/stefan_ma/DRL/DRL/Tryhard/logs/her/ur5e_reacher-v5_3/ur5e_reacher-v5 /home/stefan/Documents/Masterarbeit/DRL/Tryhard/Pulled_logs/
 
--- Tryhard/test_normalize.py
+++ Tryhard/test_normalize.py
@@ -2,14 +2,21 @@ def _normalize_scalar(var, old_min, old_max, new_min, new_max):
         """ Normalize scalar var from one range to another """
         return var / (old_max - old_min) * (new_max - new_min)
 
+def _normalize_scalar_wrong( var, old_min, old_max, new_min, new_max):
+        """ Normalize scalar var from one range to another """
+        return ((new_max - new_min) * (var - old_min) / (old_max - old_min)) + new_min
 
-old_min  = -3.0
-old_max = 4.0
 
-new_min = -0.5
-new_max = 0.5
+old_min  = -1
+old_max = 1
 
-var = -1.7
+new_min = -0.1
+new_max = 0.1
+
+var = -0.5
 
 new_var = _normalize_scalar(var, old_min, old_max,new_min,new_max)
 print (new_var)
+
+new_var_wrong = _normalize_scalar_wrong(var, old_min, old_max,new_min,new_max)
+print (new_var_wrong)
