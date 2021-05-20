598c16280c2ed28ece65e6d06a3189a43f6223fa
stefanwanckel <wanckel@gestalt-robotics.com>
2021-05-20 10:29:07 +0200

    moved logs, videos and figures to video folder. Added video for pick task

---
+++ hindsight-experience-replay-ur5/start_training.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#train the ur5_reach-v1:
+#  mpirun -np 1 python -u train.py --env-name='ur5_reach-v1' --n-cycles=10 2>&1 | tee reach.log
+#train the ur5_push-v1:
+#  mpirun -np 8 python -u train.py --env-name='ur5_push-v1' 2>&1 | tee push.log
+#train the ur5_PickAndPlace-v1:
+mpirun -np 8 python -u train.py --env-name='ur5_PickAndPlace-v1' --n-epochs=80 2>&1 | tee pick.log
+#train the FetchSlide-v1:
+#  mpirun -np 8 python -u train.py --env-name='FetchSlide-v1' --n-epochs=200 2>&1 | tee slide.log
