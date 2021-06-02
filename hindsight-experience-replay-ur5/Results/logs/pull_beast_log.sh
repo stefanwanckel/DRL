e972005ea0a304b92ec2eb20f9ee8f76fda24dc0
stefanwanckel <wanckel@gestalt-robotics.com>
2021-06-02 11:56:01 +0200

    adapted reach plot

---
+++ hindsight-experience-replay-ur5/Results/logs/pull_beast_log.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp -r 192.168.255.10:/data/stefan_ma/DRL/DRL/hindsight-experience-replay-ur5/slide_beast.log /home/stefan/Documents/Masterarbeit/DRL/hindsight-experience-replay-ur5/Results/logs
